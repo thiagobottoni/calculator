71951289ed9ada695f81b13b0083bd9bd5424c14
Thiago Bottoni <bottonithiago@gmail.com>
2023-11-23 00:05:07 -0300

    Started the calculator logic

---
+++ scripts/calc.js
@@ -0,0 +1,9 @@
+// GLOBAL VARIABLES
+// Initializes global variables
+let number1 = 0;
+let number2 = 0;
+let operator = "=";
+
+// Calculator buttons
+const calcButtons = document.querySelectorAll("button");
+
